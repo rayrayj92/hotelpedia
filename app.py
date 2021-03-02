5e02548c9ef05891cbdd1a6ae88453ac1f638f08
jinook <cowlsdnr77@gmail.com>
2021-03-02 23:57:18 +0900

    push-test-commit2

--- app.py
+++ app.py
@@ -7,5 +7,8 @@ app = Flask(__name__)
 def homework():
     return render_template('index.html')
 
+##push test
+print("push")
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
