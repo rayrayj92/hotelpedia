e28270f2082449b9ed348a1c6b28bc2ace3d520c
Chanyeop Jeong <rayrayj92@gmail.com>
2021-03-03 13:32:18 +0900

    datail page

--- app.py
+++ app.py
@@ -26,6 +26,10 @@ import hashlib
 def homework():
     return render_template('index.html')
 
+@app.route('/detail')
+def detail():
+    return render_template('detail.html')
+
 @app.route('/login')
 def login():
     return render_template('login.html')
